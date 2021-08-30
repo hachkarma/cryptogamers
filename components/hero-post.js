0181edcbcd25cf2f07aab6706cb2683f48e067ba
Victor Arnau <victor@hdesking.com>
2021-08-30 17:50:23 +0200

    minor changes

--- components/hero-post.js
+++ components/hero-post.js
@@ -13,7 +13,7 @@ export default function HeroPost({
 }) {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
+      <div className="mb-3 md:mb-16">
         <CoverImage
           title={title}
           src={coverImage}
@@ -24,19 +24,21 @@ export default function HeroPost({
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
+          <h3 className="mb-2 text-xl lg:text-2xl leading-tight font-bold">
             <Link as={`/posts/${slug}`} href="/posts/[slug]">
               <a className="hover:underline">{title}</a>
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
+          <div className="mb-2 md:mb-0 text-md">
             <DateFormatter dateString={date} />
           </div>
         </div>
         <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+          <p className="text-xs leading-relaxed mb-4 ">{excerpt}</p>
           <Avatar name={author.name} picture={author.picture} />
         </div>
+
+        <hr className="mt-2" />
       </div>
     </section>
   )
--- components/intro.js
+++ components/intro.js
@@ -1,21 +1,23 @@
 import { CMS_NAME } from '../lib/constants'
+import Image from 'next/image'
 
 export default function Intro() {
-  return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
-      </h4>
-    </section>
-  )
+	return (
+		<section className="section">
+			<div className="flex flex-row items-center section-intro">
+				<Image
+					src='/images/logo.png'
+					alt='Cryptogamers'
+					className='logo'
+					layout="fixed"
+					width='40'
+					height='40'
+				/>
+				<h1 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8 pl-4">
+					CryptoGamers
+				</h1>
+			</div>
+		
+		</section>
+	)
 }
